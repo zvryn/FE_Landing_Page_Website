21758c1990f44ec0791e82a3145db7c3be9438da
zvryn <sarinkanok@gmail.com>
2024-10-11 11:28:18 +0700

    add section component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import { curve, heroBackground, robot } from "../assets";
+import Section from "./Section";
+
+const Hero = () => {
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25rem]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
+          <h1 className="h1 mb-6">
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            <span className="inline-block relative">
+              Brainwave{" "}
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Hero;
