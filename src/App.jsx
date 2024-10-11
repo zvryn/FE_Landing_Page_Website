17efc20ca9c4f271813391472ae46823332e7bd2
zvryn <sarinkanok@gmail.com>
2024-10-11 16:34:14 +0700

    add pricing roadmap footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/RoadMap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -14,6 +17,9 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
